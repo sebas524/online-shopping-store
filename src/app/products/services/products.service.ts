286aa21fa7f1cc23f26efb782364e6048f0bd65e
sebas524 <velezjsebastian@gmail.com>
2025-11-01 19:27:00 +0100

    implement cache with ttlof 5mins

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 interface Options {
@@ -18,8 +18,22 @@ export class ProductsService {
 
   private baseUrl: string = environment.baseUrl;
 
+  private productsCache = new Map<
+    string,
+    { data: ProductsResponse; timestamp: number }
+  >();
+
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+
+    const cacheKey = `limit=${limit}&offset=${offset}&gender=${gender}`;
+
+    const cached = this.productsCache.get(cacheKey);
+
+    if (cached && Date.now() - cached.timestamp < 5 * 60 * 1000) {
+      //* cache valid for 5 minutes
+      return of(cached.data);
+    }
 
     return this.http
       .get<ProductsResponse>(`${this.baseUrl}/products`, {
@@ -32,6 +46,14 @@ export class ProductsService {
       .pipe(
         tap((response) => {
           console.log('Products fetched:', response);
+        }),
+        tap((response) => {
+          //* On first successful fetch for this key, we memoize the response,
+          //* so subsequent identical requests are served from memory.
+          this.productsCache.set(cacheKey, {
+            data: response,
+            timestamp: Date.now(),
+          });
         })
       );
   }
