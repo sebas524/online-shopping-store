6213c30c6d96f712e726c8e162fac2c88162f33f
sebas524 <velezjsebastian@gmail.com>
2025-11-07 12:07:11 +0100

    add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then((m) => {
+        return m.authRoutes;
+      }),
+  },
+
+  {
     path: '',
     loadChildren: () =>
       import('./shop-front/shop-front.routes').then((m) => {
